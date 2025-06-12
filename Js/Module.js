0a2d2f4fd98e6c746ea2a519155a6a7e52f71e4b
MonsteParty23 <lorelyn.paga@gmail.com>
2025-06-12 13:30:42 +0800

    initial commit

--- Js/Module.js
+++ Js/Module.js
@@ -1,86 +1,84 @@
-const productdb = (dbname, table) => {
-    //create database
+const productsdb = (dbname, table) => {
+  const db = new Dexie(dbname);
+  db.version(1).stores(table);
+  db.open();
 
-    const db = new Dexie(dbname)
-    db.version(1).stores(table); 
-    db.open();
+  return db;
+  /**
+       * const db = new Dexie('myDb');
+          db.version(1).stores({
+          friends: `name, age`
+      });
+       */
+};
 
-/*
-const db = new Dexie('myDb');
-db.version(1).stores({
-    friends: `name, age`
-})
-*/
-return db;
-}
+const bulkcreate = (dbtable, data) => {
+  let flag = empty(data);
+  if (flag) {
+    dbtable.bulkAdd([data]);
+    console.log("data inserted successfully...!");
+  } else {
+    console.log("Please provide data...!");
+  }
+  return flag;
+};
 
-// insert function
-   const bulkcreate = (dbtable, data) => {
-    let flag = empty(data);
-    if(flag){
-        dbtable.bulkAdd([data]);
-        console.log("Data inserted successfully");
-    }else{
-        console.log("Please provide data")
-    }
-       return flag;
-}
+// create dynamic elements
+const createEle = (tagname, appendTo, fn) => {
+  const element = document.createElement(tagname);
+  if (appendTo) appendTo.appendChild(element);
+  if (fn) fn(element);
+};
 
 // check textbox validation
-const empty = object =>{
-    let flag = false;
-    for(const value in object){
-        if(object[value] != "" && object.hasOwnProperty(value)){
-            flag = true;
-        }else{
-            flag = false;
-        }
+const empty = object => {
+  let flag = false;
+  for (const value in object) {
+    if (object[value] != "" && object.hasOwnProperty(value)) {
+      flag = true;
+    } else {
+      flag = false;
     }
-    return flag;
-}
+  }
+  return flag;
+};
 
-//get data from the database
-const getData = (dbtable, fn) =>{
-    let index = 0;
-    let obj = {};
-
-    dbtable.count((count)=>{
-        if(count){
-            dbtable.each(table=>{
-                
-                obj = Sortobj(table);
-                fn(obj, index++);
-                
-            })
-        }else{
-            fn(0);
-        }
-    })
-}
-
-//Sort object
-const Sortobj = sortobj =>{
-    let obj = {};
-    obj = {
-        id: sortobj.id,
-        name:sortobj.name,
-        seller:sortobj.seller,
-        price:sortobj.price
+// getData from the database
+const getData = (dbname, fn) => {
+  let index = 0;
+  let obj = {};
+  dbname.count(count => {
+    // count rows in the table using count method
+    if (count) {
+      dbname.each(table => {
+        // table => return the table object data
+        // to arrange order we are going to create for in loop
+        obj = SortObj(table);
+        fn(obj, index++); // call function with data argument
+      });
+    } else {
+      fn(0);
     }
-    return obj;
+  });
+};
+
+const SortObj = (sortobj) => {
+  let obj = {};
+  obj = {
+    id: sortobj.id,
+    name: sortobj.name,
+    seller: sortobj.seller,
+    price: sortobj.price
+  };
+  return obj;
 }
 
-// create dynamic element
-const createEle = (tagname, appendTo, fn) =>{
-    const element = document.createElement(tagname);
-    if(appendTo) appendTo.appendChild(element);
-    if(fn) fn(element);
-}
 
-export default productdb;
+export default productsdb;
 export {
-    bulkcreate,
-    getData,
-    createEle
+  bulkcreate,
+  createEle,
+  getData,
+  SortObj
 };
 
